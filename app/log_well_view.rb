dad47332689f046075fdb8076c7b08ba819e451d
thatmose <moseswanyonyi@gmail.com>
2016-08-10 10:37:32 -0600

    Fix config to allow running of app, but lose testing ability

--- app/log_well_view.rb
+++ app/log_well_view.rb
@@ -3,7 +3,7 @@ require 'las_reader'
 require 'json'
 require_relative 'well'
 
-class LogWellView < Sinatra::Base
+# class LogWellView < Sinatra::Base
 
   get '/' do
     erb :index
@@ -17,11 +17,11 @@ class LogWellView < Sinatra::Base
     File.open("#{@filename}", "w") {|f| f.write(Net::HTTP.get(uri)) }
     @worklas = CWLSLas.new("#{@filename}")
     @current_well = Well.new(@worklas)
-  #Write to json file
-  File.open("#{@filename}.json", "w") {|f| f.write(@current_well.welldata.to_json) }
-  
-  @current_well.welldata.to_json
+    #Write to json file
+    File.open("#{@filename}.json", "w") {|f| f.write(@current_well.welldata.to_json) }
+    
+    @current_well.welldata.to_json
 
   end
 
-end
+# end
